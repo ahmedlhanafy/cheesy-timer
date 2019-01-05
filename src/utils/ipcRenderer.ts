e35634d2e0c2204588b5f81f48b41e7dba859675
Ahmed Elhanafy <ahmed.elhanafy95@gmail.com>
2019-01-05 02:00:04 +0200

    Fix ipcRenderer

--- src/utils/ipcRenderer.ts
+++ src/utils/ipcRenderer.ts
@@ -1,4 +1,3 @@
-// import { ipcRenderer as ipc } from 'electron';
+import { ipcRenderer as ipc } from 'electron';
 
-// export const { ipcRenderer }: { ipcRenderer: typeof ipc } = global as any;
-export const ipcRenderer: any = { on: () => {}, removeListener: () => {}, once: () => {}, send: () => {} };
+export const { ipcRenderer }: { ipcRenderer: typeof ipc } = global as any;
